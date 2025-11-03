425147bc1967dfadf04f007ea9cfd8a5bbb11257
Sachin Bista <bistasachin39@gmail.com>
2025-11-03 04:12:06 +0000

    Add day01 folder and first_day.sh script

---
+++ first_day.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#This is my first shell scripting language for devOps learning
+
+echo "Hello Friends, I'm learning via TrainWithSubhnam yt channel"
+echo "This is the basic of learning using .sh in editor"
